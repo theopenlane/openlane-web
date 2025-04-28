b1e19b4b1f03d9932eadea7a14145a4c13b6da00
Mateo Kantoci <info@mateo-it.com>
2025-04-28 23:32:41 +0200

    web-6#: added cursor pointer on toggle theme

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -45,14 +45,14 @@ export default function ThemeToggle() {
       <button
         onClick={() => toggleTheme(false)}
         aria-label="Toggle Light Mode"
-        className={`w-9 h-9 flex items-center justify-center rounded-full ${!isDark ? "toggle-bg" : ""}`}
+        className={`w-9 h-9 cursor-pointer flex items-center justify-center rounded-full ${!isDark ? "toggle-bg" : ""}`}
       >
         <Sun size={18} />
       </button>
       <button
         onClick={() => toggleTheme(true)}
         aria-label="Toggle Dark Mode"
-        className={`w-9 h-9 flex items-center justify-center rounded-full ${isDark ? "toggle-bg" : ""}`}
+        className={`w-9 h-9 cursor-pointer flex items-center justify-center rounded-full ${isDark ? "toggle-bg" : ""}`}
       >
         <Moon size={18} />
       </button>
