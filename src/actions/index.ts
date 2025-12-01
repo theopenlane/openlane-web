29a6f1a1e60e972b769c28ba78bccd4036c6131e
Mateo Kantoci <info@mateo-it.com>
2025-12-01 03:00:43 +0100

    ISS-1051: implemented recaptcha

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -4,6 +4,24 @@ import { z } from "astro:schema";
 
 const resend = new Resend(import.meta.env.SECRET_RESEND_API_KEY);
 
+async function verifyRecaptcha(token: string) {
+  const secret = import.meta.env.RECAPTCHA_SECRET_KEY;
+
+  const response = await fetch(
+    "https://www.google.com/recaptcha/api/siteverify",
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams({
+        secret,
+        response: token,
+      }),
+    },
+  );
+
+  return response.json();
+}
+
 export const server = {
   send: defineAction({
     accept: "form",
@@ -12,8 +30,19 @@ export const server = {
       company: z.string(),
       email: z.string().email(),
       message: z.string(),
+      recaptchaToken: z.string(),
     }),
-    handler: async ({ name, company, email, message }) => {
+    handler: async ({ name, company, email, message, recaptchaToken }) => {
+      const result = await verifyRecaptcha(recaptchaToken);
+
+      if (!result.success || result.score < 0.3) {
+        console.error("reCAPTCHA failed:", result);
+        const err = new Error("recaptcha-failed");
+        err.name = "recaptcha-failed";
+
+        throw err;
+      }
+
       const { data, error } = await resend.emails.send({
         from: "Openlane <no-reply@mail.theopenlane.io>",
         to: "info@theopenlane.io",
@@ -21,7 +50,6 @@ export const server = {
         html: `
           <div style="font-family: sans-serif; font-size: 14px;">
             <strong>New Web Form Submission</strong><br><br>
-
             <strong>Company:</strong> ${company}<br>
             <strong>Name:</strong> ${name}<br>
             <strong>Email:</strong> ${email}<br>
