e67176db375996085fdbee95b2789f48333dd181
Bruno Kucevic <brunokucevic@gmail.com>
2026-02-27 16:01:57 +0100

    ISS-1686: added link
    Signed-off-by: Bruno Kucevic <brunokucevic@gmail.com>

--- src/components/shared/footer-config.ts
+++ src/components/shared/footer-config.ts
@@ -2,4 +2,5 @@ export const FOOTER_LINKS = {
   discord: "https://discord.gg/4fq2sxDk7D",
   linkedin: "https://www.linkedin.com/company/theopenlane/",
   github: "https://github.com/theopenlane",
+  githubIssues: "https://github.com/orgs/theopenlane/discussions/new/choose",
 };
